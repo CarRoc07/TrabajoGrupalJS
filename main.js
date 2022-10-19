0a920708a5d5a25d62b2b2ba442c5615133c371b
sebasjd <d.jsebastian@hotmail.com>
2022-10-19 20:51:37 -0300

    add query selectors for filter

--- main.js
+++ main.js
@@ -10,13 +10,21 @@
 //const _btn_agregar
 //const _cards_container
 
+const popularBtn = document.getElementById("_popular_category");
+const pizzaBtn = document.getElementById("_pizza_category");
+const burgerBtn = document.getElementById("_burger_category");
+const friesBtn = document.getElementById("_fries_category");
+const burritoBtn = document.getElementById("_burrito_category");
+const tacoBtn = document.getElementById("_taco_category");
+const smoothieBtn = document.getElementById("_smoothie_category");
+
 // Carrito
 
 const btnOpen = document.querySelector("._carrito_btnOpen");
-    btnClose = document.querySelector("._carrito_btnClose");
-    btnClose2 = document.querySelector("._carrito_container_end_close");
-    cart = document.querySelector("._carrito");
-    overlay = document.querySelector(".overlay");
+btnClose = document.querySelector("._carrito_btnClose");
+btnClose2 = document.querySelector("._carrito_container_end_close");
+cart = document.querySelector("._carrito");
+overlay = document.querySelector(".overlay");
 
 const openAndCloseCart = () => {
     cart.classList.toggle("open-cart");
