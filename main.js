3a6fb7a4366ff315a30a39f683c5fecd8e3b5d5f
sebasjd <d.jsebastian@hotmail.com>
2022-10-27 01:14:02 -0300

    Debug cantidades en LS

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ window.addEventListener("scroll", closeOnScroll);
 const renderProduct = (product) => {
 
 
-    const { id, name, precio, comentario, productImg } = product;
+    const { id, name, precio, comentario, productImg, quantity } = product;
 
     return `
     <div class="_results_container_output">
@@ -202,7 +202,7 @@ const removeProductCart = (e) => {
 }
 
 
-function addItemToCart(title, price, imageSrc, descripcion) {
+function addItemToCart(title, price, imageSrc, descripcion, cantidad) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
 
@@ -226,7 +226,8 @@ function addItemToCart(title, price, imageSrc, descripcion) {
             modalAdd2.innerHTML = `Se sumó otro <span> ${title} </span> al carrito de compras`
             var found = objetosEnCarritoLS.find(e => e.titulo == title);
             found.cantidad = sumar
-            saveLocalStorage(objetosEnCarritoLS);
+            saveLocalStorage(objetosEnCarrito);
+
 
             // const modif = (found) => { found.defineProperty(this, "cantidad", "3") }
             // modif(found)
@@ -245,15 +246,20 @@ function addItemToCart(title, price, imageSrc, descripcion) {
                         </div>
                         <div class="_carrito_container_btns">
                             <button class="btnRemoveProductCart disable">-</button>
-                            <input type="number" class="cantidad" value="1">
+                            <input type="number" class="cantidad" value=1>
                             <button class="btnAddProductCart">+</button>
                         </div>
                     </div>
         `
     cartRow.innerHTML = cartRowContents;
     cartItems.append(cartRow);
-    crearObjeto(title, price, imageSrc, descripcion);
+    crearObjeto(title, price, imageSrc, descripcion, cantidad);
     saveLocalStorage(objetosEnCarrito);
+    objetosEnCarritoLS = objetosEnCarrito
+
+
+    // objetosEnCarritoLS.forEach(e => mostrarLS(e))
+
     modalAdd1.classList.add("showModal");
     modalAdd1.innerHTML = `Se agregó un <span> ${title} </span> al carrito de compras`;
 
@@ -265,13 +271,13 @@ function addItemToCart(title, price, imageSrc, descripcion) {
 // Función crear array de objetos en carrito
 var objetosEnCarrito = [];
 
-function crearObjeto(title, price, imageSrc, descripcion, quantity) {
+function crearObjeto(title, price, imageSrc, descripcion) {
     var objeto = {
         titulo: title,
         precio: price,
         imagen: imageSrc,
         descripction: descripcion,
-        cantidad: quantity
+        cantidad: 1
     }
     objetosEnCarrito[objetosEnCarrito.length] = objeto;
 }
@@ -286,7 +292,7 @@ const mostrarLS = (e) => {
     const price = e.precio
     const imageSrc = e.imagen
     const descripcion = e.description
-    const quantity = e.cantidad
+    const cantidad = Number(e.cantidad)
     var cartItems = document.getElementsByClassName('_carrito_container_products')[0];
     var cartRow = document.createElement('div')
     cartItems.append(cartRow);
@@ -301,12 +307,13 @@ const mostrarLS = (e) => {
                         </div>
                         <div class="_carrito_container_btns">
                             <button class="btnRemoveProductCart disable">-</button>
-                            <input type="number" class="cantidad" value=${quantity}>
+                            <input type="number" class="cantidad" value=${cantidad}>
                             <button class="btnAddProductCart">+</button>
                         </div>
                     </div>
         `
     cartRow.innerHTML = cartRowContents;
+
 
 }
 
--- objetos.js
+++ objetos.js
@@ -1,6 +1,5 @@
-
-class producto   {
-    constructor(id, tipo, name, precio, comentario, productImg, popular) {
+class producto {
+    constructor(id, tipo, name, precio, comentario, productImg, popular, cantidad) {
         this.id = id;
         this.tipo = tipo;
         this.name = name;
@@ -8,41 +7,43 @@ class producto   {
         this.comentario = comentario;
         this.productImg = productImg;
         this.popular = popular;
-        
+        this.cantidad = cantidad;
+
     }
 }
 
-let _burguesa = new producto (1, 'hamburguesas', 'Ham-burguesa', 1100, 'Clase alta', './img/img-array/burguesa.jpg', false);
-let _explosive = new producto (2, 'hamburguesas', 'Explosive', 1560, 'Doble bomba', './img/img-array/explosive.jpg',false);
-let _family = new producto (3, 'hamburguesas', 'Familiy-game', 2260, 'Para repartir', './img/img-array/family.jpg',false);
-let _meat = new producto (4, 'hamburguesas', '!Meat', 1260, 'Vegana y poderosa', './img/img-array/meat.jpg',false);
-let _peppa = new producto (5, 'hamburguesas', 'Peppa-pig', 1200, 'La cerdita', './img/img-array/peppa.jpg',false);
-let _chicken = new producto (6, 'hamburguesas', 'Chicken-little', 1000, 'De pollo y cositas', './img/img-array/chicken.jpg',false);
-let _muzza = new producto (7, 'pizzas', 'Muzzaraña', 950, 'Muzza inspiradora', './img/img-array/muzza.jpg',false);
-let _morron = new producto (8, 'pizzas', 'Joe-Morrone', 1050, 'Pizza punk', './img/img-array/morron.jpg',false);
-let _tropi = new producto (9, 'pizzas', 'Tropicalisima', 1220, 'Sabor y fiesta', './img/img-array/tropi.jpg',false);
-let _fuga = new producto (10, 'pizzas', 'La-fuga', 1150, 'Fugazzeta del amor', './img/img-array/fuga.jpg',false);
-let _rucula= new producto (11, 'pizzas', 'Rucu-jam', 1380, 'Sabor y fiesta', './img/img-array/rucula.jpg',false);
-let _roquefort= new producto (12, 'pizzas', 'Don-Roque', 1260, 'Roquefort a morir', './img/img-array/roquefort.jpg',false);
-let _solari = new producto (13, 'fritas', 'Solari', 600, 'Papa y nada mas', './img/img-array/solari.jpg',false);
-let _francisco= new producto (14, 'fritas', 'Papas-Francisco', 880, 'Argentinas y celestiales', './img/img-array/francisco.jpg',false);
-let _panceta= new producto (15, 'fritas', 'Panza-verde', 850, 'Panceta y verdeo', './img/img-array/panceta.jpg',false);
-let _cheddar= new producto (16, 'fritas', 'Cheese-potato', 900, 'Cheddar y mas cheddar', './img/img-array/cheddar.jpg',false);
-let _gramajo= new producto (17, 'fritas', 'Gramajo', 1100, 'Mas revuelto q tu vida', './img/img-array/gramajo.jpg',false);
-let _horno= new producto (18, 'fritas', 'Oven', 1100, '+ sanas, = de ricas', './img/img-array/horno.jpg',false);
-let _wrap_carne = new producto (19, 'wraps', 'Meat-wrap', 1100, 'A la antigua', './img/img-array/wrap-carne.jpg',true);
-let _wrap_cerdo = new producto (20, 'wraps', 'Pig-wrap', 1100, 'Si la vaca ya te harto', './img/img-array/wrap-cerdo.jpg',false);
-let _wrap_pollo = new producto (21, 'wraps', 'Chicken_wrap', 1100, 'Para cambiar un poco', './img/img-array/wrap-pollo.jpg',false);
-let _wrap_mixto = new producto (22, 'wraps', 'Mix-wrap', 1300, 'Si no podes elegir uno', './img/img-array/wrap-mixto.jpg',true);
-let _wrap_vegan = new producto (23, 'wraps', 'Vegan-wrap', 1250, 'Pet friendly', './img/img-array/wrap-vegan.jpg',true);
-let _batido_cherry = new producto (24, 'batidos', 'Cherry-bomb', 900, 'Cerecita', './img/img-array/batido-cherry.jpg',true);
-let _batido_chocolate = new producto (25, 'batidos', 'Chocolate-lover', 900, 'Superclasico', './img/img-array/batido-chocolate.jpg',true);
-let _batido_vainilla = new producto (26, 'batidos', 'Vanilla-ice', 900, 'Ultra refresh', './img/img-array/batido-vainilla.jpg',false);
-let _batido_ddl = new producto (27, 'batidos', 'Sweet-heart', 900, 'Bombon de dulce de leche', './img/img-array/batido-ddl.jpg',true);
-let _batido_crema = new producto (28, 'batidos', 'Creamy', 900, 'Suave y cremoso', './img/img-array/batido-crema.jpg',false);
+let _burguesa = new producto(1, 'hamburguesas', 'Ham-burguesa', 1100, 'Clase alta', './img/img-array/burguesa.jpg', false, 1);
+let _explosive = new producto(2, 'hamburguesas', 'Explosive', 1560, 'Doble bomba', './img/img-array/explosive.jpg', false, 1);
+let _family = new producto(3, 'hamburguesas', 'Familiy-game', 2260, 'Para repartir', './img/img-array/family.jpg', false, 1);
+let _meat = new producto(4, 'hamburguesas', '!Meat', 1260, 'Vegana y poderosa', './img/img-array/meat.jpg', false, 1);
+let _peppa = new producto(5, 'hamburguesas', 'Peppa-pig', 1200, 'La cerdita', './img/img-array/peppa.jpg', false, 1);
+let _chicken = new producto(6, 'hamburguesas', 'Chicken-little', 1000, 'De pollo y cositas', './img/img-array/chicken.jpg', false, 1);
+let _muzza = new producto(7, 'pizzas', 'Muzzaraña', 950, 'Muzza inspiradora', './img/img-array/muzza.jpg', false, 1);
+let _morron = new producto(8, 'pizzas', 'Joe-Morrone', 1050, 'Pizza punk', './img/img-array/morron.jpg', false, 1);
+let _tropi = new producto(9, 'pizzas', 'Tropicalisima', 1220, 'Sabor y fiesta', './img/img-array/tropi.jpg', false, 1);
+let _fuga = new producto(10, 'pizzas', 'La-fuga', 1150, 'Fugazzeta del amor', './img/img-array/fuga.jpg', false, 1);
+let _rucula = new producto(11, 'pizzas', 'Rucu-jam', 1380, 'Sabor y fiesta', './img/img-array/rucula.jpg', false, 1);
+let _roquefort = new producto(12, 'pizzas', 'Don-Roque', 1260, 'Roquefort a morir', './img/img-array/roquefort.jpg', false, 1);
+let _solari = new producto(13, 'fritas', 'Solari', 600, 'Papa y nada mas', './img/img-array/solari.jpg', false, 1);
+let _francisco = new producto(14, 'fritas', 'Papas-Francisco', 880, 'Argentinas y celestiales', './img/img-array/francisco.jpg', false, 1);
+let _panceta = new producto(15, 'fritas', 'Panza-verde', 850, 'Panceta y verdeo', './img/img-array/panceta.jpg', false, 1);
+let _cheddar = new producto(16, 'fritas', 'Cheese-potato', 900, 'Cheddar y mas cheddar', './img/img-array/cheddar.jpg', false, 1);
+let _gramajo = new producto(17, 'fritas', 'Gramajo', 1100, 'Mas revuelto q tu vida', './img/img-array/gramajo.jpg', false, 1);
+let _horno = new producto(18, 'fritas', 'Oven', 1100, '+ sanas, = de ricas', './img/img-array/horno.jpg', false, 1);
+let _wrap_carne = new producto(19, 'wraps', 'Meat-wrap', 1100, 'A la antigua', './img/img-array/wrap-carne.jpg', true, 1);
+let _wrap_cerdo = new producto(20, 'wraps', 'Pig-wrap', 1100, 'Si la vaca ya te harto', './img/img-array/wrap-cerdo.jpg', false, 1);
+let _wrap_pollo = new producto(21, 'wraps', 'Chicken_wrap', 1100, 'Para cambiar un poco', './img/img-array/wrap-pollo.jpg', false, 1);
+let _wrap_mixto = new producto(22, 'wraps', 'Mix-wrap', 1300, 'Si no podes elegir uno', './img/img-array/wrap-mixto.jpg', true, 1);
+let _wrap_vegan = new producto(23, 'wraps', 'Vegan-wrap', 1250, 'Pet friendly', './img/img-array/wrap-vegan.jpg', true, 1);
+let _batido_cherry = new producto(24, 'batidos', 'Cherry-bomb', 900, 'Cerecita', './img/img-array/batido-cherry.jpg', true, 1);
+let _batido_chocolate = new producto(25, 'batidos', 'Chocolate-lover', 900, 'Superclasico', './img/img-array/batido-chocolate.jpg', true, 1);
+let _batido_vainilla = new producto(26, 'batidos', 'Vanilla-ice', 900, 'Ultra refresh', './img/img-array/batido-vainilla.jpg', false, 1);
+let _batido_ddl = new producto(27, 'batidos', 'Sweet-heart', 900, 'Bombon de dulce de leche', './img/img-array/batido-ddl.jpg', true, 1);
+let _batido_crema = new producto(28, 'batidos', 'Creamy', 900, 'Suave y cremoso', './img/img-array/batido-crema.jpg', false, 1);
 
 
-const _array_de_productos = [_burguesa, _explosive, _family, _meat, _peppa, _chicken, _muzza, 
-_morron, _tropi, _fuga, _rucula, _roquefort, _solari, _francisco, _panceta, _cheddar, _gramajo, 
-_horno, _wrap_carne, _wrap_cerdo, _wrap_pollo, _wrap_mixto, _wrap_vegan, _batido_cherry, _batido_chocolate, 
-_batido_vainilla, _batido_ddl, _batido_crema];
+const _array_de_productos = [_burguesa, _explosive, _family, _meat, _peppa, _chicken, _muzza,
+    _morron, _tropi, _fuga, _rucula, _roquefort, _solari, _francisco, _panceta, _cheddar, _gramajo,
+    _horno, _wrap_carne, _wrap_cerdo, _wrap_pollo, _wrap_mixto, _wrap_vegan, _batido_cherry, _batido_chocolate,
+    _batido_vainilla, _batido_ddl, _batido_crema
+];
